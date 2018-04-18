887b13653891c709efef1b7d6d52427f4624b5f3
Daniel Niczyporuk <niczyporuk96@interia.pl>
2018-04-18 07:38:20 +0000

    poprawka migracji 3.0

--- db/migrate/20180125084448_edit_tables.rb
+++ db/migrate/20180125084448_edit_tables.rb
@@ -1,5 +1,5 @@
-class EditTables < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :opinions, :imageable, :polymorphic => true
-  end
-end
+#class EditTables < ActiveRecord::Migration[5.2]
+  #def change
+  #  add_reference :opinions, :imageable, :polymorphic => true
+ # end
+#end
--- db/migrate/20180125092630_edit_opinions.rb
+++ db/migrate/20180125092630_edit_opinions.rb
@@ -1,6 +1,5 @@
 class EditOpinions < ActiveRecord::Migration[5.2]
   def change
-      remove_column :opinions, :imageable
       add_column :opinions, :book_id, :integer
       add_column :opinions, :user_id, :integer
   end
